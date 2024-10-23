a66410565ac5446fe50b305043f70f46c10a3494
AmarnathJJagatap <137856946+AmarnathJJagatap@users.noreply.github.com>
2024-10-23 12:03:58 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import PoseEstimation from './PoseEstimation';
+// import PoseEstimation1 from './PoseEstimation1';
+import DualCameraPoseEstimation from './DualCameraPoseEstimation';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot (document.getElementById ('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    
+    {/* <PoseEstimation /> */}
+    {/* <PoseEstimation1 />   */}
+    < DualCameraPoseEstimation / >
+  
+  
+  
+  </>
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
